3d1ba320308020071310f428b63afdb394be7405
Jessit Jorge Patiño Coronel <draco2287@gmail.com>
2023-03-11 00:14:54 -0400

    servicios de producto actualizados

--- frontEnd/src/app/services/product-service.service.ts
+++ frontEnd/src/app/services/product-service.service.ts
@@ -1,18 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from '@enviroment/environment';
-import { Product } from '@models/product';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "@enviroment/environment";
+import { Product } from "@models/product";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class ProductService {
   private baseURL: string = `${environment.API_URL}/productos`;
 
   constructor(private http: HttpClient) {}
 
-  getProductsByName(name: string) {
-    return this.http.get<Product[]>(`${this.baseURL}/buscar-productos/${name}`);
+  getProducts() {
+    return this.http.get<Product[]>(`${this.baseURL}/productos`);
   }
 
   getProductsAleatory() {
@@ -21,19 +21,27 @@ export class ProductService {
     );
   }
 
+  getProductsByName(name: string) {
+    return this.http.get<Product[]>(`${this.baseURL}/buscar-productos/${name}`);
+  }
+
+  getProductsById(id: number) {
+    return this.http.get<Product[]>(`${this.baseURL}/producto/${id}`);
+  }
+
   getProductsByCategory(category: string) {
     return this.http.get<Product[]>(`${this.baseURL}/categoria/${category}`);
   }
 
-  saveProduct(product: Product) {
+  addProduct(product: Product) {
     return this.http.post(this.baseURL, product);
+  }
+
+  updateProduct(product: Product, productId: number) {
+    return this.http.put(`${this.baseURL}/${productId}`, product);
   }
 
   delteteProduct(productId: number) {
     return this.http.delete(`${this.baseURL}/${productId}`);
-  }
-
-  updatePieceCategory(product: Product, productId: number) {
-    return this.http.put(`${this.baseURL}/${productId}`, product);
   }
 }
